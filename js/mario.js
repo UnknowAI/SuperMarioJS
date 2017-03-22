319f8d6fda11e816e14aaa4967a34bcc06d5fc0c
DevisionDev <luukwauben@hotmail.nl>
2017-03-22 23:04:25 +0100

    basics
    added small_mario sprite and nonanimated movements

--- js/mario.js
+++ js/mario.js
@@ -1,27 +1,27 @@
-// create the Scene object
-var scene = sjs.Scene({w:256, h:224});
+var mp = 2;
+var fps = 60;
+
+var scene = sjs.Scene({w:256*mp, h:224*mp});
+var input = scene.Input();
+
 var sl = "js/sprites/";
-var audio = new Audio('js/audio_file.mp3');
-audio.play();
+var mario = scene.Sprite('js/sprites/small_mario.png');
+mario.size(17, 17);
+mario.offset(0, 0);
+mario.move(40*mp, 200*mp);
+mario.scale(2);
+mario.update();
 
-// load the images in parallel. When all the images are
-// ready, the callback function is called.
-scene.loadImages([sl + 'character.png'], function() {
-    // create the Sprite object;
-    var sp = scene.Sprite('js/sprites/character.png');
-    // change the visible size of the sprite
-    sp.size(585, 727);
-    // apply the latest visual changes to the sprite
-    // (draw if canvas, update attribute if DOM);
-    sp.update();
-    // change the offset of the image in the sprite
-    // (this works the opposite way of a CSS background)
-    sp.offset(0, 0);
-    // various transformations
-    sp.move(0, 0);
-    sp.rotate(3.14 / 4);
-    sp.scale(0.2);
-    sp.setOpacity(1);
-
-    sp.update();
-});
+window.setInterval(function() {
+    if(input.keyboard.right){
+        mario.move(5,0);
+        mario.update();
+    }
+    if(input.keyboard.left){
+        mario.move(-5,0);
+        mario.update();
+    }
+    if(input.keyboard.up){
+        
+    }
+}, 1000/fps);
