78f15600858426ab53cb85a23c1792d63b768ebb
Grey <luukwauben@hotmail.nl>
2017-05-01 12:18:51 +0200

    added flip on mario, now flips horizontally when changing direction.
    BUG: Mario is blurred when facing left

--- js/game.js
+++ js/game.js
@@ -1,16 +1,20 @@
 window.setInterval(function() {             // game loop
-    if(input.keyboard.right){               // check if player moves right
+    if(input.keyboard.right) {               // check if player moves right
+        small_mario_direction = "r";
         if(small_mario.x <= width -20) {    // check if player is not at end of scene
+            flipMario();
             small_mario.move(5,0);          // move player right
         }
     }
-    if(input.keyboard.left){                // check if player moves left
+    if(input.keyboard.left) {                // check if player moves left
+        small_mario_direction = "l";
         if(small_mario.x >= 11) {           // check if player is not at beginning of scene
+            flipMario();
             small_mario.move(-5,0);         // move player left
         }
     }
-    if(input.keyboard.up){                  // checks if player jumps
-
+    if(input.keyboard.up || input.keyboard.space){                  // checks if player jumps
+        
     }
     update();
 }, 1000/fps);                               // tickrate
--- js/mario.js
+++ js/mario.js
@@ -1,6 +1,22 @@
 var small_mario = scene.Sprite('js/sprites/small_mario.png');   // small mario sprite
+var small_mario_direction = "r";
+var small_mario_flip = 0;
 
 small_mario.size(17, 17);                                       // small mario size in pixels
 small_mario.offset(0, 0);
 small_mario.move(40*mp, 200*mp);                                // small mario position
 small_mario.scale(mp);                                          // small mario scale
+
+function flipMario() {
+    if(small_mario_direction == "r") {
+        if(small_mario_flip != 0) {
+            small_mario_flip = 0;
+            small_mario.dom.style.transform = "scale(2, 2) rotatey(" + small_mario_flip + "deg)";
+        }
+    } else {
+        if(small_mario_flip != 180) {
+            small_mario_flip = 180;
+            small_mario.dom.style.transform = "scale(2, 2) rotatey(" + small_mario_flip + "deg)";
+        }
+    }
+}
